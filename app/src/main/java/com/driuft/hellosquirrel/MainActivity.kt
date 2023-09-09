86ad3a5176bb1766486f1287e1fadb7a704ad3a6
ShaneEDouglas <112830171+ShaneEDouglas@users.noreply.github.com>
2023-09-09 00:15:39 -0400

    Initial Commit with bio card and layout

--- app/src/main/java/com/driuft/hellosquirrel/MainActivity.kt
+++ app/src/main/java/com/driuft/hellosquirrel/MainActivity.kt
@@ -1,37 +1,81 @@
 package com.driuft.hellosquirrel
 
+import android.graphics.ImageDecoder
+import android.graphics.drawable.AnimatedImageDrawable
+import android.graphics.drawable.AnimationDrawable
+import android.graphics.drawable.Drawable
 import android.os.Build
 import android.os.Bundle
 import android.view.WindowInsets
 import android.view.WindowManager
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.content.res.AppCompatResources
+import com.bumptech.glide.Glide
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var profileImage: ImageView
     private lateinit var profileName: TextView
     private lateinit var profileBio: TextView
+    private lateinit var profilegif: ImageView
+    private lateinit var secprofilegif: ImageView
 
+
+    @RequiresApi(Build.VERSION_CODES.P)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         configureViews()
         setupProfile()
+        circleImage()
+        setupGifs()
+
     }
 
     private fun configureViews() {
         profileImage = findViewById(R.id.profile_image)
         profileName = findViewById(R.id.profile_name)
         profileBio = findViewById(R.id.profile_bio)
+        profilegif = findViewById(R.id.star)
+        secprofilegif = findViewById(R.id.secstar)
     }
 
     private fun setupProfile() {
         configureImage()
         configureName()
+    }
+
+    private fun circleImage() {
+        Glide.with(this)
+            .load(R.drawable.sddev)
+            .circleCrop()
+            .into(profileImage)
+    }
+
+    @RequiresApi(Build.VERSION_CODES.P)
+    private fun setupGifs() {
+//
+//        val source = ImageDecoder.createSource(resources, R.drawable.betterstargif)
+//        val drawable: Drawable = ImageDecoder.decodeDrawable(source)
+//        profilegif.setImageDrawable(drawable)
+//        (drawable as? AnimatedImageDrawable)?.start()
+
+
+        Glide.with(this)
+            .asGif()
+            .load(R.drawable.betterstargif)
+            .into(profilegif)
+
+        Glide.with(this)
+            .asGif()
+            .load(R.drawable.betterstargif)
+            .into(secprofilegif)
+
+
     }
 
     private fun configureImage() {
